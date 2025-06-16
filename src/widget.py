c8ccc5ea20a96f44e4d3b8a03bc9d7ce4e1040c8
Egor_Poplavskiy <epoplavskiy223@gmail.com>
2025-06-16 11:04:33 +0300

    add widget.py

---
+++ src/widget.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(number_cart: str) -> str | tuple[str, int | str]:
+    """Функция которая обрабатывает информацию о карте и счёте"""
+    if "счет" in number_cart.lower().split()[0]:
+        return f"Счет {get_mask_account(number_cart.split()[-1])}"
+    else:
+        return " ".join(number_cart.split()[:-1]), get_mask_card_number(number_cart.split()[-1])
+
+
+def get_date(original_format_date: str) -> str:
+    """Форматирует дату"""
+    date_obj = datetime.strptime(original_format_date, "%Y-%m-%dT%H:%M:%S.%f")
+    date = date_obj.strftime("%d.%m.%Y")
+    return date
--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,5 @@
 from src.masks import get_mask_account, get_mask_card_number
+from src.widget import get_date, mask_account_card
 
 number_cart = "Счет 35383033474447895560"
 original_format_date = "2024-03-11T02:26:18.671407"
@@ -8,3 +9,9 @@ if __name__ == "__main__":
 
 if __name__ == "__main__":
     print(get_mask_account(number_cart))
+
+if __name__ == "__main__":
+    print(mask_account_card(number_cart))
+
+if __name__ == "__main__":
+    print(get_date(original_format_date))
