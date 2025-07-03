db25f3146ac5af00f11fffb8f6c9926c93d20a37
Egor_Poplavskiy <epoplavskiy223@gmail.com>
2025-07-03 03:32:57 +0300

    add test_mask_account_card

---
+++ tests/test_mask_account_card.py
@@ -0,0 +1,23 @@
+import pytest
+
+from src.widget import mask_account_card
+
+
+@pytest.mark.parametrize(
+    'original, expected',
+    [
+        ('Счет 5351711234567890', 'Счет **7890'),
+        ('Счет 4222222222222121', '**2121'),
+        ('60111111111111', '**1111'),
+        ('37828224631000', '**1000'),
+        ("1234567890", "Ошибка! Счет не найдет!"),
+        ("", "Ошибка! Счет не найдет!"),
+        ("Счет", "Ошибка! Счет не найдет!")
+
+    ]
+)
+
+def test_mask_account_card(original, expected):
+    """ Проверка отличия счета от карт """
+    masked = mask_account_card(original)
+    assert masked == expected
